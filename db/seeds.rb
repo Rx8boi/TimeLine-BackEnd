ea401c5ab4ffa660e2751cce62f2a1e13bfaba76
Charles Butler <cdbutle@outlook.com>
2021-03-10 18:14:25 -0600

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,46 +15,58 @@ Event.create([
 
     date: "01/02/2020",
     title: "Moved to Austin",
-    event_type: "life event",
-    summary: "did stuff",
+    event_type: "Life Event",
+    summary: "Packed up from El Paso to start a new career.",
     skills_learned: "lorem ",
 },
 
 {  #2
 
-    date: "01/02/2021",
-    title: "moved to el paso",
-    event_type: "life event",
-    summary: "did things",
-    skills_learned: "nada ",
+    date: "08/10/2020",
+    title: "Started Flatiron",
+    event_type: "Education",
+    summary: "Full Stack Software Engineering Bootcamp. Learned RoR, React.js, Javascript",
+    skills_learned: "stuff ",
 },
 
 {  #3
 
-    date: "01/10/2020",
-    title: "worked at VA",
-    event_type: "work history",
-    summary: "did things for money",
+    date: "11/10/2020",
+    title: "GoodLooking Software Engineer",
+    event_type: "Work history",
+    summary: "Started working as Junior SWE at start up company",
     skills_learned: "how to work",
 },
 
 {  #4
 
-    date: "11/02/2010",
-    title: "worked at goodlooking",
-    event_type: "work history",
-    summary: "got paid",
-    skills_learned: "how to not job search",
+    date: "02/17/2021",
+    title: "Flatiron Graduation",
+    event_type: "Education",
+    summary: "Utilized culmination of Flatiron Education to create final project & graduated",
+    skills_learned: "alot",
 },
 
 {  #5
 
-    date: "09/12/2018",
-    title: "Flatiron Bootcamp",
-    event_type: "education",
-    summary: "got some coding skills",
-    skills_learned: "lots of things",
+    date: "03/01/2021",
+    title: "Began Post-Flatiron Job Search",
+    event_type: "Life Event",
+    summary: "Met with Career Counselor weekly and applied for jobs",
+    skills_learned: "how to work",
 },
+
+{  #6
+
+    date: "03/19/2021",
+    title: "Deletion Test",
+    event_type: "Test",
+    summary: "Click the backspace icon to delete this card",
+    skills_learned: "how to work",
+},
+
+
+
 ])
 
 Job.create ([
